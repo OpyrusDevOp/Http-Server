0b45732c4aeed1cd41d2adc4b5ef37944cdb607d
OpyrusDevOp <opyrusdeveloper@gmail.com>
2024-11-14 03:48:42 +0100

    Init

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+var tcpServer = new TcpListener(IPAddress.Any, 3000);
+
+tcpServer.Start();
+
+while (true)
+{
+    var socket = tcpServer.AcceptSocket();
+    var msg = System.Text.Encoding.UTF8.GetBytes("Connection Succeed !!");
+    socket.Send(msg, 0, msg.Length, SocketFlags.None);
+    socket.Close();
+}
